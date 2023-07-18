fda7f405a34ba88bb86e3b02657c2dfc08607261
NMTuan <nmtuan@qq.com>
2023-07-18 17:01:06 +0800

    feat: 完成客户端和服务端的基础逻辑

--- laf-cloud/functions/__heartbeat_client_test.ts
+++ laf-cloud/functions/__heartbeat_client_test.ts
@@ -1,7 +1,128 @@
-
+/*
+ * @Author: NMTuan
+ * @Email: NMTuan@qq.com
+ * @Date: 2023-07-18 09:29:53
+ * @LastEditTime: 2023-07-18 16:55:07
+ * @LastEditors: NMTuan
+ * @Description:
+ * @FilePath: \laf_heartbeat\laf-cloud\functions\__heartbeat_client_test.ts
+ */
 import cloud from '@lafjs/cloud'
 
+const heartBeatConfig = {
+    serverUrl: '',
+    serverKey: ''
+}
+
+interface Action {
+    [key: string]: {
+        methods: ('GET' | 'POST')[]
+        auth: boolean
+        handler: Function
+    }
+}
+
+// 配置项
+const config = {
+    name: 'laf_heartbeat_c_test', // 当前应用名称
+    collectionPrefix: 'laf_heartbeat_c_test', // 数据库前缀
+    sharedPrefix: 'laf_heartbeat_c_test' // 全局缓存前缀
+}
+
+// 统一数据库集合的名称
+// 使用 collection('user') 访问数据库时，则会去访问 firefly_user 数据集合。
+// 保证各项目之间数据库独立，且不容易混淆。
+const collection = (name) => {
+    if (name) {
+        return cloud.database().collection(`${config.collectionPrefix}_${name}`)
+    } else {
+        return cloud.database().collection(`${config.collectionPrefix}`)
+    }
+}
+
+// 统一缓存前缀
+// 同样保证各项目之间缓存独立，不易混淆。
+const shared = {
+    get(key) {
+        return cloud.shared.get(`${config.sharedPrefix || ''}_${key}`)
+    },
+    set(key, value) {
+        return cloud.shared.set(`${config.sharedPrefix || ''}_${key}`, value)
+    }
+}
+
+const actions: Action = {}
+
+// 获取 key
+actions.getKey = {
+    methods: ['GET'],
+    auth: false,
+    async handler(ctx) {
+        return {
+            code: 20000,
+            data: {
+                key: heartBeatConfig.serverKey
+            }
+        }
+    }
+}
+
+// 发送心跳
+export const heartBeat = async () => {
+    if (!heartBeatConfig.serverUrl || !heartBeatConfig.serverKey) {
+        console.log('heartBeat error: no set heartBeatConfig')
+        return
+    }
+
+    const sended = await cloud.fetch({
+        url: heartBeatConfig.serverUrl,
+        params: {
+            action: 'receive',
+            key: heartBeatConfig.serverKey
+        }
+    })
+    if (sended.code !== 20000) {
+        console.log('heartBeat error: server receive error', sended.message)
+    }
+    return sended.data
+}
+
+// 用接口模拟定时器，仅测试用
+// actions.heartBeat = {
+//     methods: ['GET'],
+//     auth: false,
+//     handler: heartBeat
+// }
+
+// 入口，拦截器
 export default async function (ctx: FunctionContext) {
-  console.log('Hello World')
-  return { data: 'hi, laf' }
+    const { action } = ctx.query
+
+    // 黑名单处理和访问频率处理，此处省去。见帖子：https://forum.laf.run/d/130/7
+    // 顺便说一下，黑名单的处理可以放在`__interceptor__`中，这里仅处理频率问题即可。
+
+    // 没有 action 或没找到 action 报错
+    if (!action || !actions[action]) {
+        return { code: 40000 }
+    }
+
+    const { methods, auth, handler } = actions[action]
+
+    // method 不符
+    if (!methods.includes(ctx.method)) {
+        return { code: 40000 }
+    }
+
+    // 鉴权
+    if (auth && ctx.user === null) {
+        return { code: 40100 }
+    }
+
+    // 执行
+    if (handler) {
+        return handler(ctx)
+    }
+
+    // 能执行到这里肯定哪儿写错了
+    return { code: 50000 }
 }
--- laf-cloud/functions/__heartbeat_server_test.ts
+++ laf-cloud/functions/__heartbeat_server_test.ts
@@ -1,7 +1,454 @@
-
 import cloud from '@lafjs/cloud'
 
+interface Action {
+    [key: string]: {
+        methods: ('GET' | 'POST')[]
+        auth: boolean
+        handler: Function
+    }
+}
+
+// 配置项
+const config = {
+    name: 'laf_heartbeat_s_test', // 当前应用名称
+    collectionPrefix: 'laf_heartbeat_s_test', // 数据库前缀
+    sharedPrefix: 'laf_heartbeat_s_test' // 全局缓存前缀
+}
+
+// 统一数据库集合的名称
+// 使用 collection('user') 访问数据库时，则会去访问 firefly_user 数据集合。
+// 保证各项目之间数据库独立，且不容易混淆。
+const collection = (name?: string) => {
+    if (name) {
+        return cloud.database().collection(`${config.collectionPrefix}_${name}`)
+    } else {
+        return cloud.database().collection(`${config.collectionPrefix}`)
+    }
+}
+
+// 统一缓存前缀
+// 同样保证各项目之间缓存独立，不易混淆。
+const shared = {
+    get(key) {
+        return cloud.shared.get(`${config.sharedPrefix || ''}_${key}`)
+    },
+    set(key, value) {
+        return cloud.shared.set(`${config.sharedPrefix || ''}_${key}`, value)
+    }
+}
+
+const actions: Action = {}
+
+// 注册 client 信息
+actions.signup = {
+    methods: ['POST'],
+    auth: false,
+    async handler(ctx) {
+        const { clientUrl } = ctx.body
+        if (!clientUrl) {
+            return { code: 40000 }
+        }
+        // 先检查数据库是否已存在
+        const exits = await collection()
+            .where({
+                clientUrl
+            })
+            .getOne()
+        // 存在则返回整个数据
+        if (exits.data !== null) {
+            return {
+                code: 20000,
+                data: exits.data
+            }
+        }
+        // 不存在则随机生成key，并插入新数据
+        const key = (Date.now() * Math.random()).toString(36)
+        const inserted = await collection().add({
+            key, // 认证key
+            clientUrl, // 客户端地址
+            sendUrl: '', // 发送通道
+            errorMax: 3, // 最大异常次数
+            errorCount: 0, //当前异常次数
+            noticeMax: 3, // 最大通知次数
+            noticeCount: 0, // 当前通知次数
+            state: 0, // 0未激活 1已激活-待生效 2已生效
+            paused: 0, //是否暂停服务 1暂停 0正常
+            createdAt: Date.now(), // 创建时间
+            updatedAt: Date.now() // 更新时间
+        })
+        // 最后把整个数据返回
+        const item = await collection().doc(inserted.id).get()
+        return {
+            code: 20000,
+            data: item.data
+        }
+    }
+}
+
+// 激活 client 信息
+actions.active = {
+    methods: ['POST'],
+    auth: false,
+    async handler(ctx) {
+        const { id } = ctx.body
+        if (!id) {
+            return { code: 40000 }
+        }
+        try {
+            const item = await collection().doc(id).get()
+            if (item.data === null) {
+                return {
+                    code: 40000,
+                    message: 'not found'
+                }
+            }
+            if (item.data.state !== 0) {
+                return {
+                    code: 40000,
+                    message: 'No need to activate again'
+                }
+            }
+            const { data: client } = await cloud.fetch({
+                url: item.data.clientUrl,
+                params: {
+                    action: 'getKey'
+                }
+            })
+            if (client.code !== 20000) {
+                return { code: 50000 }
+            }
+            if (item.data.key !== client.data.key) {
+                return { code: 40000, message: 'The client key is incorrect' }
+            }
+            const updated = await collection().doc(item.data._id).update({
+                state: 1
+            })
+            return {
+                code: 20000,
+                data: {
+                    ...item.data,
+                    state: 1
+                }
+            }
+        } catch (err) {
+            return {
+                code: 50000,
+                message: err.message
+            }
+        }
+
+        // return new Promise(async (resolve, reject) => {
+        //     if (!id) {
+        //         return resolve({ code: 40000 })
+        //     }
+        //     // 先查状态
+        //     collection()
+        //         .doc(id)
+        //         .get()
+        //         .then(({ data }) => {
+        //             if (data.state !== 0) {
+        //                 return resolve({
+        //                     code: 20000,
+        //                     message: '已激活'
+        //                 })
+        //           }
+        //           // 获取 client 的 key
+        //             return cloud.fetch({
+        //                 url: data.clientUrl,
+        //                 params: {
+        //                     action: 'getKey'
+        //                 }
+        //             })
+        //         })
+        //         .then((res) => {
+        //             if (res.code !== 20000 || !res.data.serverKey) {
+        //                 resolve({ code: 50000 })
+        //           }
+        //           if()
+        //             const key = res.data.serverKey
+        //         })
+        //         .catch((err) => {
+        //             return resolve({
+        //                 code: 50000,
+        //                 message: err
+        //             })
+        //         })
+
+        // cloud
+        //     .fetch({
+        //         url: clientUrl,
+        //         params: {
+        //             action: 'getKey'
+        //         }
+        //     })
+        //     .then((res) => {
+        //         if (res.code !== 20000 || !res.data.serverKey) {
+        //             resolve({ code: 50000 })
+        //         }
+        //         const key = res.data.serverKey
+        //     })
+        //     .catch((err) => {
+        //         return resolve({
+        //             code: 50000,
+        //             message: err
+        //         })
+        //     })
+        // })
+    }
+}
+
+// 切换 paused 状态
+actions.toggle = {
+    methods: ['POST'],
+    auth: false,
+    async handler(ctx) {
+        const { id } = ctx.body
+        if (!id) {
+            return { code: 40000 }
+        }
+        const toggled = await collection()
+            .doc(id)
+            .update({
+                $bit: {
+                    paused: {
+                        xor: 1
+                    }
+                }
+            })
+
+        if (!toggled.ok) {
+            return { code: 50000 }
+        }
+        const item = await collection().doc(id).get()
+        return {
+            code: 20000,
+            data: item.data
+        }
+    }
+}
+
+// 删除
+actions.remove = {
+    methods: ['POST'],
+    auth: false,
+    async handler(ctx) {
+        const { id } = ctx.body
+        if (!id) {
+            return { code: 40000 }
+        }
+        const removed = await collection().doc(id).remove()
+        if (!removed.ok) {
+            return { code: 50000 }
+        }
+        return { code: 20000 }
+    }
+}
+
+// 更新配置
+actions.update = {
+    methods: ['POST'],
+    auth: false,
+    async handler(ctx) {
+        const { id, payload } = ctx.body
+        if (!id) {
+            return { code: 40000 }
+        }
+        const allowKeys = ['sendUrl', 'errorMax', 'noticeMax']
+        const params = allowKeys.reduce((total, key) => {
+            if (payload[key] !== undefined) {
+                total[key] = payload[key]
+            }
+            return total
+        }, {})
+        const updated = await collection().doc(id).update(params)
+        if (!updated.ok) {
+            return { code: 50000 }
+        }
+        const item = await collection().doc(id).get()
+
+        return {
+            code: 20000,
+            data: item.data
+        }
+    }
+}
+
+// 接收心跳
+actions.receive = {
+    methods: ['GET'],
+    auth: false,
+    async handler(ctx) {
+        const { key } = ctx.query
+        if (!key) {
+            return {
+                code: 40000,
+                message: 'missing parameter'
+            }
+        }
+        const updated = await collection()
+            .where({
+                key
+            })
+            .update({
+                errorCount: 0, // 重置计数器
+                noticeCount: 0, // 重置计数器
+                state: 2, // 更新状态为：已生效
+                updatedAt: Date.now() // 更新更新时间
+            })
+        return {
+            code: 20000,
+            data: updated
+        }
+    }
+}
+
+// 定时任务
+export const schedule = async () => {
+    // 找到需要发通知的数据
+    const noticeItems = await collection()
+        .where({
+            $and: [
+                { state: 2 },
+                { paused: 0 },
+                {
+                    sendUlr: {
+                        $ne: ''
+                    }
+                },
+                // errorCount >= errorMax
+                {
+                    $expr: {
+                        $gte: ['$errorCount', '$errorMax']
+                    }
+                },
+                // noticeCount < noticeMax
+                {
+                    $expr: {
+                        $lt: ['$noticeCount', '$noticeMax']
+                    }
+                },
+                {
+                    updatedAt: {
+                        $lt: Date.now() - 60 * 1000
+                    }
+                }
+            ]
+        })
+        .get()
+
+    // 所有需要发通知的 noticeCount++
+    const noticeItemInc = await collection()
+        .where({
+            $and: [
+                { state: 2 },
+                { paused: 0 },
+                {
+                    sendUlr: {
+                        $ne: ''
+                    }
+                },
+                // errorCount >= errorMax
+                {
+                    $expr: {
+                        $gte: ['$errorCount', '$errorMax']
+                    }
+                },
+                // noticeCount < noticeMax
+                {
+                    $expr: {
+                        $lt: ['$noticeCount', '$noticeMax']
+                    }
+                },
+                {
+                    updatedAt: {
+                        $lt: Date.now() - 60 * 1000
+                    }
+                }
+            ]
+        })
+        .update({
+            $inc: {
+                noticeCount: 1
+            }
+        })
+
+    // 所有异常的 errorCount++
+    const errorItemInc = await collection()
+        .where({
+            $and: [
+                { state: 2 },
+                { paused: 0 },
+                {
+                    sendUlr: {
+                        $ne: ''
+                    }
+                },
+                // errorCount < errorMax
+                {
+                    $expr: {
+                        $lt: ['$errorCount', '$errorMax']
+                    }
+                },
+                { noticeCount: 0 },
+                {
+                    updatedAt: {
+                        $lt: Date.now() - 60 * 1000
+                    }
+                }
+            ]
+        })
+        .update({
+            $inc: {
+                errorCount: 1
+            }
+        })
+
+    // 发通知
+    noticeItems.data.map((item) => {
+        cloud.fetch(item.sendUrl)
+    })
+    return {
+        code: 20000,
+        data: noticeItems.data
+    }
+}
+
+// 用接口模拟定时器，仅测试用
+// actions.schedule = {
+//     methods: ['GET'],
+//     auth: false,
+//     handler: schedule
+// }
+
+// 入口，拦截器
 export default async function (ctx: FunctionContext) {
-  console.log('Hello World')
-  return { data: 'hi, laf' }
+    const { action } = ctx.query
+
+    // 黑名单处理和访问频率处理，此处省去。见帖子：https://forum.laf.run/d/130/7
+    // 顺便说一下，黑名单的处理可以放在`__interceptor__`中，这里仅处理频率问题即可。
+
+    // 没有 action 或没找到 action 报错
+    if (!action || !actions[action]) {
+        return { code: 40000 }
+    }
+
+    const { methods, auth, handler } = actions[action]
+
+    // method 不符
+    if (!methods.includes(ctx.method)) {
+        return { code: 40000 }
+    }
+
+    // 鉴权
+    if (auth && ctx.user === null) {
+        return { code: 40100 }
+    }
+
+    // 执行
+    if (handler) {
+        return handler(ctx)
+    }
+
+    // 能执行到这里肯定哪儿写错了
+    return { code: 50000 }
 }
